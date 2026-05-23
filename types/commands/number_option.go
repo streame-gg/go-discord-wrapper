61e9e973531ede83bca0545919a4bfd1e06bbd0e
DGamet <gamet@streame.gg>
2026-05-23 16:15:16 +0200

    Fix: linter by running gomft

--- types/commands/number_option.go
+++ types/commands/number_option.go
@@ -6,16 +6,16 @@ import (
 )
 
 type ApplicationCommandOptionNumber struct {
-	Type                     discord.ApplicationCommandOptionType     `json:"type"`
-	Name                     string                                   `json:"name"`
-	NameLocalizations        map[discord.Locale]string                `json:"name_localizations,omitempty"`
-	Description              string                                   `json:"description"`
-	DescriptionLocalizations map[discord.Locale]string                `json:"description_localizations,omitempty"`
-	Required                 *bool                                    `json:"required,omitempty"`
+	Type                     discord.ApplicationCommandOptionType      `json:"type"`
+	Name                     string                                    `json:"name"`
+	NameLocalizations        map[discord.Locale]string                 `json:"name_localizations,omitempty"`
+	Description              string                                    `json:"description"`
+	DescriptionLocalizations map[discord.Locale]string                 `json:"description_localizations,omitempty"`
+	Required                 *bool                                     `json:"required,omitempty"`
 	Choices                  []ApplicationCommandOptionChoice[float64] `json:"choices,omitempty"`
 	MinValue                 *float64                                  `json:"min_value,omitempty"`
 	MaxValue                 *float64                                  `json:"max_value,omitempty"`
-	Autocomplete             *bool                                    `json:"autocomplete,omitempty"`
+	Autocomplete             *bool                                     `json:"autocomplete,omitempty"`
 }
 
 func (o *ApplicationCommandOptionNumber) ApplicationCommandOptionType() discord.ApplicationCommandOptionType {
